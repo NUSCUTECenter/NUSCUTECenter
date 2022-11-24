496074d2a402b5ba5b524ca852c989bd003d9a78
Daniel Ho <hkwdaniel@gmail.com>
2022-11-24 12:51:14 +0800

    testing on root domain

---
+++ __settings__.js
@@ -0,0 +1,22 @@
+ASSET_PREFIX = "";
+SCRIPT_PREFIX = "";
+SCENE_PATH = "1597105.json";
+CONTEXT_OPTIONS = {
+    'antialias': true,
+    'alpha': true,
+    'preserveDrawingBuffer': false,
+    'preferWebGl2': true,
+    'powerPreference': "high-performance"
+};
+SCRIPTS = [ 112030375, 112030355, 112030356, 112030351, 112030378, 112030388, 112030362, 112030367, 112030380, 112030389, 112030361, 112030360, 112030392, 112030393, 112030402, 112030408, 112030409, 112030431, 112030450, 112030476, 112030477, 112030479, 112030481, 112030524, 112030533, 112030554, 112030555, 112030556, 112030575, 112030577, 112030583, 112030600, 112030601 ];
+CONFIG_FILENAME = "config.json";
+INPUT_SETTINGS = {
+    useKeyboard: true,
+    useMouse: true,
+    useGamepads: false,
+    useTouch: true
+};
+pc.script.legacy = false;
+PRELOAD_MODULES = [
+    {'moduleName' : 'Ammo', 'glueUrl' : 'files/assets/112030590/1/ammo.wasm.js', 'wasmUrl' : 'files/assets/112030589/1/ammo.wasm.wasm', 'fallbackUrl' : 'files/assets/112030588/1/ammo.js', 'preload' : true},
+];
